e679df4770ea37e5df6d0b79f43d833fc76d87c6
NOOBHECMETAHA <ebaltboupotha@gmail.com>
2024-03-25 12:46:04 +0300

    asd

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <img alt="Vue logo" class="logo box" :class="{ 'jump': isJumping }" src="./assets/AdminLTELogo.png" width="125" height="125" />
+  <div>
+    <p class="header">Количество нажатий: {{ this.count}}</p>
+    <vue-button label="Нажми на меня" @click="addClick"></vue-button>
+  </div>
+</template>
+
+<script>
+import Button from 'primevue/button';
+
+export default {
+    data() {
+        return {
+            count: 0,
+            isJumping: false
+        };
+    },
+    components: {
+      'vue-button': Button
+    },
+    methods: {
+      addClick(){
+        this.count++;
+
+        this.isJumping = true;
+
+        setTimeout(() => {
+          this.isJumping = false;
+        }, 500); // Установите время анимации здесь
+      }
+    }
+};
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.box {
+  transition: transform 0.5s;
+}
+
+.jump {
+  transform: translateY(-20px); /* Расстояние, на которое объект будет подпрыгивать */
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import PrimeVue from 'primevue/config';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).use(PrimeVue).mount('#app')
